cd05c53e0a3d4f9f923669ef02700a1cfea0e047
Daniil Streltsov <streltsovdaniil00@gmail.com>
2025-01-12 20:22:53 +0100

    Create main.py

---
+++ chess/main.py
@@ -0,0 +1,100 @@
+import sys
+import pygame as p
+
+BOARD_WIDTH = BOARD_HEIGHT = 512
+MOVE_LOG_PANEL_WIDTH = 250
+MOVE_LOG_PANEL_HEIGHT = BOARD_HEIGHT
+DIMENSION = 8
+SQ_SIZE = BOARD_HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+# Add these constants after your existing constants
+BUTTON_WIDTH = 100
+BUTTON_HEIGHT = 40
+BUTTON_MARGIN = 10
+BUTTON_COLOR = (119, 153, 82)
+BUTTON_TEXT_COLOR = (255, 255, 255)
+
+
+
+# Define colors
+
+# 1 Green
+
+LIGHT_SQUARE_COLOR = (237, 238, 209)
+DARK_SQUARE_COLOR = (119, 153, 82)
+MOVE_HIGHLIGHT_COLOR = (84, 115, 161)
+POSSIBLE_MOVE_COLOR = (255, 255, 51)
+
+# 2 Brown
+
+'''
+LIGHT_SQUARE_COLOR = (240, 217, 181)
+DARK_SQUARE_COLOR = (181, 136, 99)
+MOVE_HIGHLIGHT_COLOR = (84, 115, 161)
+POSSIBLE_MOVE_COLOR = (255, 255, 51)
+'''
+
+# 3 Gray
+
+'''
+LIGHT_SQUARE_COLOR = (220,220,220)
+DARK_SQUARE_COLOR = (170,170,170)
+MOVE_HIGHLIGHT_COLOR = (84, 115, 161)
+POSSIBLE_MOVE_COLOR = (164,184,196)
+'''
+
+
+def loadImages():
+    pieces = ['bR', 'bN', 'bB', 'bQ', 'bK',
+              'bp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'wp']
+    for piece in pieces:
+        image_path = "images1/" + piece + ".png"
+        original_image = p.image.load(image_path)
+        # p.transform.smoothscale is bit slower than p.transform.scale, using this to reduce pixelation and better visual quality for scaling images to larger sizes
+        IMAGES[piece] = p.transform.smoothscale(
+            original_image, (SQ_SIZE, SQ_SIZE))
+
+def main():
+    p.init()
+    screen = p.display.set_mode((BOARD_WIDTH + MOVE_LOG_PANEL_WIDTH, BOARD_HEIGHT))
+    clock = p.time.Clock()
+
+    screen.fill(p.Color(LIGHT_SQUARE_COLOR))
+    moveLogFont = p.font.SysFont("Times New Roman", 12, False, False)
+    
+    loadImages()
+    running = True
+    board = [
+        ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+        ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+        ["--", "--", "--", "--", "--", "--", "--", "--"],
+        ["--", "--", "--", "--", "--", "--", "--", "--"],
+        ["--", "--", "--", "--", "--", "--", "--", "--"],
+        ["--", "--", "--", "--", "--", "--", "--", "--"],
+        ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+        ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+    ]
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+
+        drawPieces(screen, board)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+
+
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            piece = board[row][col]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(
+                    col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+# if we import main then main function wont run it will run only while running this file
+if __name__ == "__main__":
+    main()
